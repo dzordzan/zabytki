f25845e6f4098bafa0741ced6b04256ac6a0e77c
Andrzej Piszczek <andpis58@gmail.com>
2015-06-09 19:58:46 +0200

    Merge branch 'master' of https://github.com/dzordzan/zabytki
    Conflicts:
    	.idea/gradle.xml
    	app/src/main/AndroidManifest.xml

--- app/src/main/java/com/example/andrzej/zabytki/PlaceActivity.java
+++ app/src/main/java/com/example/andrzej/zabytki/PlaceActivity.java
@@ -1,15 +1,22 @@
 package com.example.andrzej.zabytki;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.app.SearchManager;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -18,8 +25,28 @@ import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.common.UserRecoverableException;
 import com.google.android.gms.location.places.ui.PlacePicker;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-public class PlaceActivity extends Activity {
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class PlaceActivity extends Activity implements onResponse {
     private TextView name;
     private TextView addres;
     private TextView number;
@@ -33,7 +60,7 @@ public class PlaceActivity extends Activity {
         setContentView(R.layout.activity_place);
 
 
-        name= (TextView)findViewById(R.id.textView);
+        name = (TextView) findViewById(R.id.textView);
         addres = (TextView) findViewById(R.id.textView2);
         number = (TextView) findViewById(R.id.textView3);
         url = (TextView) findViewById(R.id.textView4);
@@ -42,13 +69,13 @@ public class PlaceActivity extends Activity {
 
         DatabaseHandler db = new DatabaseHandler(this);
         Log.i("aaaa", PlaceData.ID);
-        if(db.getPlace(PlaceData.ID)){
+        if (db.getPlace(PlaceData.ID)) {
             addOrDel.setText("Usuń z ulubionych");
-        }else{
+        } else {
             addOrDel.setText("Dodaj do ulubionych");
         }
 
-        if(PlaceData.NAME == null){
+        if (PlaceData.NAME == null) {
             onBackPressed();
             Context context = getApplicationContext();
             CharSequence text = "Przepraszamy ale wystąpił problem";
@@ -56,28 +83,36 @@ public class PlaceActivity extends Activity {
 
             Toast toast = Toast.makeText(context, text, duration);
             toast.show();
-        }
-        else{
+        } else {
             name.setText(PlaceData.NAME);
             addres.setText(PlaceData.ADDRESS);
 
-            if(PlaceData.PHONE_NUMBER != null){
+            if (PlaceData.PHONE_NUMBER != null) {
                 number.setText(PlaceData.PHONE_NUMBER);
-               // number.setClickable(true);
-            }else
+                // number.setClickable(true);
+            } else
                 number.setText("Brak Numeru Tel");
-               // number.setClickable(false);
-            if(PlaceData.URI != null){
+            // number.setClickable(false);
+            if (PlaceData.URI != null) {
                 url.setText(PlaceData.URI);
                 url.setClickable(true);
-            }
-            else {
+            } else {
                 url.setText("Brak Url");
                 url.setClickable(false);
             }
 
             rating.setRating(PlaceData.RATING);
 
+
+            String URL = null;
+            try {
+                URL = "https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=" + URLEncoder.encode(PlaceData.NAME + " " + PlaceData.ADDRESS, "UTF-8");
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+            AsyncFetch parkingInfoFetch = new AsyncFetch(this);
+            parkingInfoFetch.setOnResponse(this);
+            parkingInfoFetch.execute(URL);
         }
 
     }
@@ -106,9 +141,9 @@ public class PlaceActivity extends Activity {
 
     public void navigation(View view) {
 
-        String latitude  = Double.toString(PlaceData.LATLNG.latitude);
+        String latitude = Double.toString(PlaceData.LATLNG.latitude);
         String longitude = Double.toString(PlaceData.LATLNG.longitude);
-        String navigateTo = "google.navigation:q=" + latitude +"," + longitude;
+        String navigateTo = "google.navigation:q=" + latitude + "," + longitude;
         //Uri gmmIntentUri = Uri.parse("google.navigation:q=Taronga+Zoo,+Sydney+Australia");
         Uri gmmIntentUri = Uri.parse(navigateTo);
         Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
@@ -120,7 +155,7 @@ public class PlaceActivity extends Activity {
         DatabaseHandler db = new DatabaseHandler(this);
         addOrDel = (Button) findViewById(R.id.button2);
 
-        if(db.getPlace(PlaceData.ID) == false){
+        if (db.getPlace(PlaceData.ID) == false) {
             Context context = getApplicationContext();
             CharSequence text = "Obiekt został dodany do ulubionych";
             int duration = Toast.LENGTH_SHORT;
@@ -131,7 +166,7 @@ public class PlaceActivity extends Activity {
             // dodawanie do bd
             addOrDel.setText("Usuń z ulubionych");
             db.addPlace();
-        }else{
+        } else {
             //delete
             db.deletePlace(PlaceData.ID);
 
@@ -167,5 +202,100 @@ public class PlaceActivity extends Activity {
 
         Context context = getApplicationContext();
         startActivityForResult(builder.build(context), PLACE_PICKER_REQUEST);
+    }
+
+    @Override
+    public void onResponse(JSONObject object) {
+
+
+        try {
+            Log.d("TEST", "aa"+ object.getString("responseData"));
+            Pattern p = Pattern.compile("url\":\"([^\"]+)\"");
+            Matcher m = p.matcher(object.getString("responseData"));
+            while (m.find()) {
+                String url = m.group(1).replace("\\","");
+                new DownloadImageTask((ImageView) findViewById(R.id.imageView))
+                        .execute(url);
+
+                break;
+            }
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
+class AsyncFetch extends AsyncTask<String, Void, JSONObject> {
+
+    public AsyncFetch(Context context) {
+        this.context = context;
+    }
+
+    private Context context;
+    private JSONObject jsonObject;
+    private onResponse onResponse;
+
+    public onResponse getOnResponse() {
+        return onResponse;
+    }
+
+    public void setOnResponse(onResponse onResponse) {
+        this.onResponse = onResponse;
+    }
+
+    @Override
+    protected JSONObject doInBackground(String... params) {
+        try {
+
+            HttpGet get = new HttpGet(params[0]);
+            HttpClient client = new DefaultHttpClient();
+
+            HttpResponse response = client.execute(get);
+            HttpEntity entity = response.getEntity();
+            String result = EntityUtils.toString(entity);
+            jsonObject = new JSONObject(result);
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return jsonObject;
+    }
+
+    @Override
+    protected void onPostExecute(JSONObject result) {
+        super.onPostExecute(result);
+        this.onResponse.onResponse(result);
+    }
+}
+interface onResponse {
+    public void onResponse(JSONObject object);
+}
+class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+    ImageView bmImage;
+
+    public DownloadImageTask(ImageView bmImage) {
+        this.bmImage = bmImage;
+    }
+
+    protected Bitmap doInBackground(String... urls) {
+        String urldisplay = urls[0];
+        Bitmap mIcon11 = null;
+        try {
+            InputStream in = new java.net.URL(urldisplay).openStream();
+            mIcon11 = BitmapFactory.decodeStream(in);
+        } catch (Exception e) {
+            Log.e("Error", e.getMessage());
+            e.printStackTrace();
+        }
+        return mIcon11;
+    }
+
+    protected void onPostExecute(Bitmap result) {
+        bmImage.setImageBitmap(result);
     }
 }
